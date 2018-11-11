ccc8785d4b324d569e8a6c3681e9bc56bc6b1ee7
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-11-11 16:38:58 +0200

    Put number of bombs label inside a class variable
    This is done so it could be deleted once the game ends.

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -62,7 +62,8 @@ class Board(GridLayout):
         self.insert_numbers()
         # for i in self.board:
         #   self.add_widget(i)#add the cells on the board (the View board)'''
-        self.add_widget(Label(text='number of bombs {}'.format(self.bombs), font_size='20sp'))
+        self.number_of_bombs_label = Label(text='number of bombs {}'.format(self.bombs), font_size='20sp')
+        self.add_widget(self.number_of_bombs_label)
 
     def insert_bombs(self):  # we have written this but the students won't get this
         for row, column in product(range(self.cols), range(self.cols)):
