15f089410d1298a38738ca9c1ebe1c0973837d22
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-11-18 16:34:18 +0200

    Add another test of  surrounding images

--- minesweeper/test.py
+++ minesweeper/test.py
@@ -8,3 +8,7 @@ def test_surrounding_cells():
     assert set(board.surrounding_cells(Cell(0, 0, board))) == {(0, 1), (1, 0), (1, 1)}
     assert set(board.surrounding_cells(Cell(1, 0, board))) == {(0, 0), (0, 1), (1, 1), (2, 1), (2, 0)}
     assert set(board.surrounding_cells(Cell(9, 4, board))) == {(9, 3), (8, 3), (8, 4), (8, 5), (9, 5)}
+
+    b = Board(6)
+    assert set(b.surrounding_cells(Cell(1, 2, b))) == {(0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (2, 2),
+                                                       (1, 1), (2, 1)}
