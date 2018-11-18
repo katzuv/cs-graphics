28e6dd9f4c7e715776aacfae130a3c723dd1ba6d
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-11-18 16:37:55 +0200

    Put number of columns inside a constant

--- minesweeper/grid_maker.py
+++ minesweeper/grid_maker.py
@@ -1,5 +1,6 @@
+NUM_OF_COLUMNS = 10
 with open('grid.txt', 'w') as f:
-    for row in xrange(10):
-        for column in xrange(10):
+    for row in xrange(NUM_OF_COLUMNS):
+        for column in xrange(NUM_OF_COLUMNS):
             f.write('{}{} '.format(row, column))
         f.write('\n')
